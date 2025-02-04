2783b87dad9c9d010f81738e8790daf4a40149eb
fares-kh <fares.khaled22@hotmail.com>
2025-02-04 12:25:27 +0000

    add styling for buttons

--- src/components/Holidays.jsx
+++ src/components/Holidays.jsx
@@ -1,8 +1,12 @@
 import * as React from "react"
 import './Holidays.css'
+import { TiSortAlphabetically } from "react-icons/ti";
+import { HiCurrencyPound } from "react-icons/hi2";
+import { FaStar } from "react-icons/fa";
 
 const HolidayList = () => {
     const [holidayData, setHolidayData] = React.useState([])
+    const [activeSort, setActiveSort] = React.useState()
     
     React.useEffect(() => {
         const fetchData = async () => {
@@ -30,39 +34,63 @@ const HolidayList = () => {
             return 0
         })
         setHolidayData(sortedData)
+        setActiveSort("alphabetical")
     }
 
     const sortPrice = () => {
         const sortedData = [...holidayData].sort((a, b) => a.bookingDetails.price.amount-b.bookingDetails.price.amount)
         setHolidayData(sortedData)
+        setActiveSort("price")
     }
 
     const sortRating = () => {
         const sortedData = [...holidayData].sort((a, b) => b.resort.starRating-a.resort.starRating)
         setHolidayData(sortedData)
+        setActiveSort("rating")
     }
 
-    return (<div className="holiday-container">
-        <div>
-            <button onClick={sortAlphabetically}>sort alphabetically</button>
-            <button onClick={sortPrice}>sort by price</button>
-            <button onClick={sortRating}>sort by rating</button>
-        </div>
-        <div>
+    return (
+    <div>
         {holidayData.length === 0 ? (
-          <p>Loading</p>
-        ) : (
-        holidayData.map((holiday) => (
-            <>
-            <p>{holiday.resort.name}</p>
-            <p>{holiday.resort.regionName}</p>     
-            <p>{holiday.resort.countryName}</p>
-            <p>{holiday.resort.starRating}</p>
-            <p>{holiday.resort.overview}</p>
-            <p>{holiday.bookingDetails.price.amount}</p>
-            </>
-        )))}
+            <p>Loading...</p>
+            ) : (
+        <div className="holiday-container">
+            <div className="left-col">
+                <div className="button-container">
+                    <button 
+                        className={`${activeSort === "alphabetical" ? "active" : ""}`} 
+                        onClick={sortAlphabetically}
+                    >
+                        sort alphabetically <TiSortAlphabetically className="button-icon"/>
+                    </button>
+                    <button 
+                        className={`${activeSort === "price" ? "active" : ""}`}
+                        onClick={sortPrice}
+                    >
+                        sort by price <HiCurrencyPound className="button-icon"/>
+                    </button>
+                    <button 
+                        className={`${activeSort === "rating" ? "active" : ""}`}
+                        onClick={sortRating}
+                    >
+                        sort by rating <FaStar className="button-icon"/>
+                    </button>
+                </div>
+            </div>
+            <div>
+            {holidayData.map((holiday) => (
+                <>
+                <p>{holiday.resort.name}</p>
+                <p>{holiday.resort.regionName}</p>     
+                <p>{holiday.resort.countryName}</p>
+                <p>{holiday.resort.starRating}</p>
+                <p>{holiday.resort.overview}</p>
+                <p>{holiday.bookingDetails.price.amount}</p>
+                </>
+            ))}
+            </div>
         </div>
+            )}
     </div>
     )
 }
